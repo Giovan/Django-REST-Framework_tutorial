26ed57e88d4f99a79137ba547637314d1c91543a
Giovanni <giovanniyatze@gmail.com>
2018-12-09 00:40:10 -0600

    gunicorn deploy test

--- apiconfig/settings.py
+++ apiconfig/settings.py
@@ -146,6 +146,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
 django_heroku.settings(locals())
